37449b5251e3ff7465ef38fc21b38e4722c6fe42
JGREENERT <greenerj@mail.gvsu.edu>
2015-03-26 14:12:05 -0400

    Added most of the functionality of logging into Pig Game by accessing the database.

---
+++ dropTable.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<head>
+    <title>PIGDB Drop Table</title>
+</head>
+<body>
+<?php
+/*Creating the mysql Table*/
+function createTable()
+{
+    $link = mysqli_connect("cis.gvsu.edu", "greenerj", "greenerj1234", "greenerj");
+
+    if(mysqli_connect_errno()){
+        echo mysqli_connect_error();
+        exit();
+    }
+
+    mysqli_query($link, "DROP TABLE pigDB") or die(mysqli_error($link));
+
+    echo("Table Dropped<br>");
+
+    mysqli_close($link);
+}
+createTable();
+?>
+</body>
+</html>
